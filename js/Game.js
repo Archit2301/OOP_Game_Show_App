cfeba1b96e878badf50da22490e2b80dbb3ab543
Archit Rana <archit2301@gmail.com>
2019-10-24 17:32:40 -0400

    Part 1 completed

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+   constructor() {
+     this.missed = 0;
+     this.phrases = [
+                     new Phrase('May the Force be with you'),
+                     new Phrase('Love means never having to say that you are sorry'),
+                     new Phrase('After all tomorrow is another day'),
+                     new Phrase('I feel the need the need for speed'),
+                     new Phrase('Bravo six going dark'),
+                   ];
+     this.activePhrase = null;
+   }
+
+   getRandomPhrase() {
+     const randomNumber = Math.floor(Math.random() * this.phrases.length);
+
+       return this.phrases[randomNumber];
+       this.getRandomPhrase();    
+   }
+
+   startGame() {
+     document.getElementById('overlay').style.display = "none";
+     const randomPhrase = this.getRandomPhrase();
+     this.activePhrase = randomPhrase;
+     randomPhrase.addPhraseToDisplay();
+   }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+   constructor(phrase) {
+     this.phrase = phrase.toLowerCase();
+   }
+
+   addPhraseToDisplay() {
+     const ulDiv = document.querySelector('#phrase ul');
+     const letters = this.phrase.split("");
+     for( let i = 0; i < letters.length; i++ ) {
+       const list = document.createElement('LI');
+       const listText = document.createTextNode(letters[i]);
+       list.appendChild(listText);
+       if ( letters[i].match(/^[a-z]$/) ) {
+         list.className = `hide letter ${letters[i]}`;
+       } else if ( letters[i] === " " ) {
+         list.className = "hide space";
+       }
+       ulDiv.appendChild(list);
+     }
+   }
+ }
